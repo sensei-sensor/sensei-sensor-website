d0e5d72d7cce381c0ce4a3f798f7a9bd8f8a8997
Naoki Takahashi <aho.mickey.mein@gmail.com>
2021-01-06 11:21:08 +0900

    APIから取得したデータを入れることができた

--- JS/script.js
+++ JS/script.js
@@ -2,15 +2,19 @@
 
 $('#get').on('click', () => {
   $.ajax({
-    url: 'http://vm.local:3000/website',
+    url: 'http://localhost:3000/website',
     dataType: 'JSON',
     type: 'GET'
   })
     .done((res) => {
-      console.log(res);
-      console.log(typeof (res));
-      let senseiData = JSON.parse(res);
-      $('#info').html(senseiData);
+      for (let key in res) {
+        let time = new Date(res[key].created_at).toLocaleString();
+        $('#info').text(time);
+        $('#info').text(res[key].user_name);
+        $('#info').text(res[key].room_name);
+        $('#info').text(res[key].address);
+      }
+      console.log(res[0]);
     })
     .fail((err) => {
       $('#info').html(err);
