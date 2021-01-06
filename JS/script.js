666fa02552dbd03ca9e031ce58624e9c06fb253b
Naoki Takahashi <aho.mickey.mein@gmail.com>
2021-01-06 22:26:46 +0900

    dateFormat内を実用的な方にした

--- JS/script.js
+++ JS/script.js
@@ -10,10 +10,10 @@ function get() {
   })
     .done((res) => {
       for (let key in res) {
-        let created_at = dateFormat.format(new Date(res[key].created_at), 'yyyy/MM/dd hh:mm');
+        let createdAt = dateFormat.format(new Date(res[key].created_at), 'yyyy/MM/dd hh:mm');
         $('.user_name').text(res[key].user_name);
         $('#address').text(res[key].room_name);
-        $('#created_at').text(created_at);
+        $('#created_at').text(createdAt);
       }
       console.log(res[0]);
     })
@@ -22,17 +22,29 @@ function get() {
     });
 };
 
-let dateFormat = {
-  _fmt : {
-    "yyyy": function(date) { return date.getFullYear() + ''; },
-    "MM": function(date) { return ('0' + (date.getMonth() + 1)).slice(-2); },
-    "dd": function(date) { return ('0' + date.getDate()).slice(-2); },
-    "hh": function(date) { return ('0' + date.getHours()).slice(-2); },
-    "mm": function(date) { return ('0' + date.getMinutes()).slice(-2); },
-    "ss": function(date) { return ('0' + date.getSeconds()).slice(-2); }
-  },
-  _priority : ["yyyy", "MM", "dd", "hh", "mm", "ss"],
-  format: function(date, format){
-    return this._priority.reduce((res, fmt) => res.replace(fmt, this._fmt[fmt](date)), format)
-  }
-};
+// 参考: https://qiita.com/egnr-in-6matroom/items/37e65bb642d2e158804c
+
+let dateFormat = 
+{
+ _fmt : {
+   hh: function(date) { return ('0' + date.getHours()).slice(-2); },
+   h: function(date) { return date.getHours(); },
+   mm: function(date) { return ('0' + date.getMinutes()).slice(-2); },
+   m: function(date) { return date.getMinutes(); },
+   ss: function(date) { return ('0' + date.getSeconds()).slice(-2); },
+   dd: function(date) { return ('0' + date.getDate()).slice(-2); },
+   d: function(date) { return date.getDate(); },
+   s: function(date) { return date.getSeconds(); },
+   yyyy: function(date) { return date.getFullYear() + ''; },
+   yy: function(date) { return date.getYear() + ''; },
+   t: function(date) { return date.getDate()<=3 ? ["st", "nd", "rd"][date.getDate()-1]: 'th'; },
+   w: function(date) {return ["Sun", "$on", "Tue", "Wed", "Thu", "Fri", "Sat"][date.getDay()]; },
+   MMMM: function(date) { return ["January", "February", "$arch", "April", "$ay", "June", "July", "August", "September", "October", "November", "December"][date.getMonth()]; },
+   MMM: function(date) {return ["Jan", "Feb", "$ar", "Apr", "$ay", "Jun", "Jly", "Aug", "Spt", "Oct", "Nov", "Dec"][date.getMonth()]; },  
+   MM: function(date) { return ('0' + (date.getMonth() + 1)).slice(-2); },
+   M: function(date) { return date.getMonth() + 1; },
+   $: function(date) {return 'M';}
+ },
+ _priority : ["hh", "h", "mm", "m", "ss", "dd", "d", "s", "yyyy", "yy", "t", "w", "MMMM", "MMM", "MM", "M", "$"],
+ format: function(date, format){return this._priority.reduce((res, fmt) => res.replace(fmt, this._fmt[fmt](date)), format)}
+}
