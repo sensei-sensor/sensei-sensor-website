d0f7c58a177b7e5a7c3fa54bb1871664a5481e19
Naoki Takahashi <aho.mickey.mein@gmail.com>
2021-01-07 11:27:11 +0900

    jQueryを使って、カードを増やすことができた

--- JS/script.js
+++ JS/script.js
@@ -10,12 +10,9 @@ function get() {
   })
     .done((res) => {
       for (let key in res) {
-        let createdAt = dateFormat.format(new Date(res[key].created_at), 'yyyy/MM/dd hh:mm');
-        $('.user_name').text(res[key].user_name);
-        $('#address').text(res[key].room_name);
-        $('#created_at').text(createdAt);
+        let createdAt = dateFormat.format(new Date(res[key].created_at), 'yyyy/MM/dd hh:mm');\
+        $('<div class="card"><div class="user_name">' + res[key].user_name + '</div><div class="infomation"><div class="low"><div class="description">現在地:</div><div id="address" class="data">' + res[key].room_name + '</div></div><div class="low"><div class="description">更新時刻:</div><div id="created_at" class="data">' + createdAt + '</div></div></div></div>').appendTo('.main')
       }
-      console.log(res[0]);
     })
     .fail((err) => {
       $('#info').html(err);
@@ -24,27 +21,27 @@ function get() {
 
 // 参考: https://qiita.com/egnr-in-6matroom/items/37e65bb642d2e158804c
 
-let dateFormat = 
+let dateFormat =
 {
- _fmt : {
-   hh: function(date) { return ('0' + date.getHours()).slice(-2); },
-   h: function(date) { return date.getHours(); },
-   mm: function(date) { return ('0' + date.getMinutes()).slice(-2); },
-   m: function(date) { return date.getMinutes(); },
-   ss: function(date) { return ('0' + date.getSeconds()).slice(-2); },
-   dd: function(date) { return ('0' + date.getDate()).slice(-2); },
-   d: function(date) { return date.getDate(); },
-   s: function(date) { return date.getSeconds(); },
-   yyyy: function(date) { return date.getFullYear() + ''; },
-   yy: function(date) { return date.getYear() + ''; },
-   t: function(date) { return date.getDate()<=3 ? ["st", "nd", "rd"][date.getDate()-1]: 'th'; },
-   w: function(date) {return ["Sun", "$on", "Tue", "Wed", "Thu", "Fri", "Sat"][date.getDay()]; },
-   MMMM: function(date) { return ["January", "February", "$arch", "April", "$ay", "June", "July", "August", "September", "October", "November", "December"][date.getMonth()]; },
-   MMM: function(date) {return ["Jan", "Feb", "$ar", "Apr", "$ay", "Jun", "Jly", "Aug", "Spt", "Oct", "Nov", "Dec"][date.getMonth()]; },  
-   MM: function(date) { return ('0' + (date.getMonth() + 1)).slice(-2); },
-   M: function(date) { return date.getMonth() + 1; },
-   $: function(date) {return 'M';}
- },
- _priority : ["hh", "h", "mm", "m", "ss", "dd", "d", "s", "yyyy", "yy", "t", "w", "MMMM", "MMM", "MM", "M", "$"],
- format: function(date, format){return this._priority.reduce((res, fmt) => res.replace(fmt, this._fmt[fmt](date)), format)}
+  _fmt: {
+    hh: function (date) { return ('0' + date.getHours()).slice(-2); },
+    h: function (date) { return date.getHours(); },
+    mm: function (date) { return ('0' + date.getMinutes()).slice(-2); },
+    m: function (date) { return date.getMinutes(); },
+    ss: function (date) { return ('0' + date.getSeconds()).slice(-2); },
+    dd: function (date) { return ('0' + date.getDate()).slice(-2); },
+    d: function (date) { return date.getDate(); },
+    s: function (date) { return date.getSeconds(); },
+    yyyy: function (date) { return date.getFullYear() + ''; },
+    yy: function (date) { return date.getYear() + ''; },
+    t: function (date) { return date.getDate() <= 3 ? ["st", "nd", "rd"][date.getDate() - 1] : 'th'; },
+    w: function (date) { return ["Sun", "$on", "Tue", "Wed", "Thu", "Fri", "Sat"][date.getDay()]; },
+    MMMM: function (date) { return ["January", "February", "$arch", "April", "$ay", "June", "July", "August", "September", "October", "November", "December"][date.getMonth()]; },
+    MMM: function (date) { return ["Jan", "Feb", "$ar", "Apr", "$ay", "Jun", "Jly", "Aug", "Spt", "Oct", "Nov", "Dec"][date.getMonth()]; },
+    MM: function (date) { return ('0' + (date.getMonth() + 1)).slice(-2); },
+    M: function (date) { return date.getMonth() + 1; },
+    $: function (date) { return 'M'; }
+  },
+  _priority: ["hh", "h", "mm", "m", "ss", "dd", "d", "s", "yyyy", "yy", "t", "w", "MMMM", "MMM", "MM", "M", "$"],
+  format: function (date, format) { return this._priority.reduce((res, fmt) => res.replace(fmt, this._fmt[fmt](date)), format) }
 }
